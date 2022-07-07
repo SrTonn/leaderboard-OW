d6155dad8cb0b22b03d0288c64b1b6fcc915f8f3
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-07-06 21:15:20 -0300

    refactor: replace funcs
    remove 'getHigherTierLength' and 'fill' funcs and add 'removeBattleTagsWithErros'

--- src/func/index.js
+++ src/func/index.js
@@ -4,22 +4,15 @@ const sortByHigherSR = (obj) => {
   return obj;
 };
 
-const getHigherTierLength = (array) => array.reduce((acc, { competitive }) => {
-  const tierLength = Object.keys(competitive).reduce((prev, key) => (
-    prev < competitive[key].tier.length ? competitive[key].tier.length : prev), 0);
-  return acc < tierLength ? tierLength : acc;
-}, 0);
-
-const fill = (item, minLength, char = ' ') => (
-  item.length < minLength ? item + char.repeat(minLength - item.length) : item);
+const removeBattleTagsWithErros = (obj) => !obj.error;
 
 const splitNameByClass = (array) => {
-  const tierLength = getHigherTierLength(array);
-  const result = array.reduce((acc, { name, competitive }) => {
+  const validBattleTagsList = array.filter(removeBattleTagsWithErros);
+  const result = validBattleTagsList.reduce((acc, { name, competitive }) => {
     Object.keys(competitive).forEach((key) => {
       acc[key] = [
         ...acc[key] || '',
-        `${competitive[key].rank} ${fill(competitive[key].tier, tierLength)} - ${name}`,
+        `${competitive[key].rank} ${competitive[key].tier} - ${name}`,
       ];
     });
     return acc;
